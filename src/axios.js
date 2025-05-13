75a9764878aac7bb2f0a9d05d13ba0790b2855fe
jayanth131 <adapa.joshkumar7241@gmail.com>
2025-05-13 15:57:53 +0530

    Initial commit - React + Express backend

--- src/axios.js
+++ src/axios.js
@@ -1,11 +1,9 @@
-// axios.js
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'http://127.0.0.1:5001/clone-5af24/us-central1/api/', // ✅ Make sure this is YOUR Firebase project ID
+  baseURL: 'http://localhost:5000', // 👈 Updated to your standalone server
 });
 
-// Optional: Debug request logging
 instance.interceptors.request.use((request) => {
   console.log("📦 Axios Request:", request.url, request.params || request.data);
   return request;
