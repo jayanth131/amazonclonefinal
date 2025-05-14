fc0c80c5eb651ef1d6f06aa8f4300b805361e128
ADAPA JAYANTH KUMAR <158571555+jayanth131@users.noreply.github.com>
2025-05-14 09:59:48 +0530

    Update axios.js

--- front-end/src/axios.js
+++ front-end/src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: 'https://amazonclone-final-rnyu.onrender.com/api/orders'
+  baseURL: 'http://localhost:5000', // 👈 Updated to your standalone server
 });
 
 instance.interceptors.request.use((request) => {
